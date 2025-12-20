f388034c91253740bb209dd174407a8bb16a21c4
treda87-sys <treda87@googlemail.com>
2025-12-20 12:27:32 +0100

    Create basic Unity project structure for Super Mario-style clone

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,10 @@
+// Placeholder script for the CameraFollow
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        // Camera follow logic here
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+// Placeholder script for the PlayerController
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        // Initialization code here
+    }
+
+    void Update()
+    {
+        // Player movement logic here
+    }
+}
